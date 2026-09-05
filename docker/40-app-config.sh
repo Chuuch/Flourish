1a3d2c93be8e1655b1c5249449ffd669b3e69257
Daniel Chuchulev <daniel.chuchulev96@gmail.com>
2026-09-05 18:57:18 +0300

    feat: optional sentry behind logger

--- docker/40-app-config.sh
+++ docker/40-app-config.sh
@@ -4,10 +4,16 @@ set -eu
 : "${APP_API_URL:?APP_API_URL is required}"
 : "${APP_ENV:=production}"
 
+SENTRY_DSN_JSON="undefined"
+if [ -n "${APP_SENTRY_DSN:-}" ]; then
+  SENTRY_DSN_JSON="\"${APP_SENTRY_DSN}\""
+fi
+
 cat > /usr/share/nginx/html/config.js <<EOF
 window.__APP_CONFIG__ = Object.freeze({
   API_URL: "${APP_API_URL}",
-  APP_ENV: "${APP_ENV}"
+  APP_ENV: "${APP_ENV}",
+  SENTRY_DSN: ${SENTRY_DSN_JSON}
 });
 EOF
 
